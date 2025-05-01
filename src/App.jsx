922661af0a2b191e594a4d70143322cd2777d600
bwinamit <bwinamit19@gmail.com>
2025-05-01 12:59:26 +0530

    Foundation Created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <div >
+        <p class="text-3xl font-bold underline">
+          Hello Everyone Lets Build some cool stuff with React and Vite
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
