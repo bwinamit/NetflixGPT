9f9f7285bf8ae75919b53c7785df7211a5f4f631
bwinamit <bwinamit19@gmail.com>
2025-05-07 22:05:08 +0530

    started Building of GPT functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,11 @@ import { auth } from "../Utils/Firebase";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect } from 'react'
-import {onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from '../Utils/userSlice'
 import { useDispatch } from 'react-redux'
 import { NetFLixLogo } from "../Utils/constants";
+import { toggleGptSearch } from "../Utils/gptSlice";
 
 const Header = () => {
   const user = useSelector((store) => store.user);
@@ -16,6 +17,7 @@ const Header = () => {
   const dispatch = useDispatch()
 
   const handleSignOut = () => {
+
     signOut(auth)
       .then(() => {
         navigate("/");
@@ -24,12 +26,16 @@ const Header = () => {
         console.error("Sign-out error:", error);
       });
   };
+const handleToggleSearch = () => {
+    dispatch(toggleGptSearch())
+  }
+
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        const {uid,email,displayName,photoURL} = user;
-        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
         navigate("/browse");
       } else {
         dispatch(removeUser());
@@ -45,10 +51,11 @@ const Header = () => {
     <div className="absolute top-0 left-0 w-full flex justify-between items-center p-4 z-20">
       {/* Netflix Logo */}
       <img src={NetFLixLogo} alt="Netflix Logo" className="w-40" />
-      
+
       {/* User Icon + Sign Out */}
       {user && (
         <div className="flex items-center space-x-4">
+          <button className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded cursor-pointer" onClick={handleToggleSearch}>Gpt Search</button>
           <img src={user.photoURL} alt="User Icon" className="w-10 h-10 rounded-full" />
           <button
             onClick={handleSignOut}
